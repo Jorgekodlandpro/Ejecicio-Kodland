f933b735d4d5ee78136264bc661fef56c99c450a
Jorgekodlandpro <Jorgedgm14@hotmail.com>
2024-05-09 17:21:43 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+meme_dict = {
+    "boomer": "Una persona que no esta desactualizada",
+    "bugeado": "Algo que no esta funcionando",
+    "bro": "Una amistad, o tu hermano,
+    "troll": "Una persona cuando hace muchas bromas, o es graciosa"
+    "hater": "alguien que te odia o que te tiene envidia.
+    
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+print("Hola! Bienvenido a tu diccionario de palabras modernas")
+
+
+if word in meme_dixt.keys():
+    print(meme_dict[word])
+else:
+    print("Lo Siento en mi sistema no esta esa palabra pero la agregaremos al sistema")
+}
